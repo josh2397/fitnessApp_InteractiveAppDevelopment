9b42f1d2b0a6570b96762aae4f70889e2fb2014a
josh2397 <josh23797@gmail.com>
2018-04-17 20:35:33 +1000

    Refactored Home and List page to Login and Home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
+  selector: 'page-list',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
   }
 
---
+++ src/pages/login/login.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HomePage } from "../home/home";
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  username:any;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  login() {
+    this.navCtrl.push(HomePage, {
+      username: this.username
+    });
+  }
+
+}
