ef2e0fcee58a2ef9a5cef56a8e82eb0885d1080f
josh2397 <josh23797@gmail.com>
2018-04-18 18:34:41 +1000

    Log weight page added

---
+++ src/pages/logweight/logweight.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { LogweightPage } from './logweight';
+
+@NgModule({
+  declarations: [
+    LogweightPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(LogweightPage),
+  ],
+})
+export class LogweightPageModule {}
